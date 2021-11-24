42b41a0b73d23bf4573470b465126ffb93144a88
pozi-pc <serg.pozi@gmail.com>
2021-11-24 17:15:51 +0200

    com-pc-02

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+
+            clearActiveClasses();
+
+            slide.classList.add('active');
+        })
+    }
 
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
+    function clearActiveClasses () {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 
-        clearActiveClasses();
-
-        slide.classList.add('active');
-    })
 }
 
-
-function clearActiveClasses () {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-}
-
-
-
-
+slidesPlugin(1);
 
 
 
